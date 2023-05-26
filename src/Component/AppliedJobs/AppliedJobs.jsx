9ecf519065a791361be4ede3e7745fe40b70c529
pinkir <pinki.roy@outlook.com>
2023-05-26 16:40:57 +0600

    check

--- src/Component/AppliedJobs/AppliedJobs.jsx
+++ src/Component/AppliedJobs/AppliedJobs.jsx
@@ -9,14 +9,21 @@ const AppliedJobs = () => {
     return (
         <div className=''>
             <h1 className='text-4xl text-center font-bold relative -top-28'>Applied Jobs</h1>
+            <div className=" ml-14 dropdown dropdown-right">
+                <label tabIndex={0} className="btn m-1 bg-purple-300 text-black">Filter</label>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-40">
+                    <li><a>Onsite</a></li>
+                    <li><a>Full-time</a></li>
+                </ul>
+            </div>
             {
                 jobCart.map(job => <Review
-                key = {job._id}
-                job = {job}
+                    key={job._id}
+                    job={job}
                 ></Review>)
             }
-            
-            
+
+
         </div>
     );
 };
